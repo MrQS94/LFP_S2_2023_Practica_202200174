6b67e0057f0f45021075447edddbca875426bd93
MrQS94 <quezale@outlook.com>
2023-08-03 21:51:19 -0600

    Avance 1.03

--- ProductoDAO.py
+++ ProductoDAO.py
@@ -12,22 +12,19 @@ class Producto_DAO():
             with open(os.getcwd() + "\\Lab LFP\\Practica 1\\data.inv" , 'r', encoding='UTF-8') as archivo:
                 lineas = archivo.readlines()
                 for linea in lineas:
-                    instruccion, detalles = linea.strip().split(' ', 1)
+                    try:
+                        instruccion, detalles = linea.strip().split(' ', 1)
+                    except ValueError:
+                        print('EL archivo .inv está mal escrito.')
+                        print('Recordar usar el siguiente ejemplo:')
+                        print('crear_producto <nombre>;<cantidad>;<precio_unitario>;<ubicacion>')
                     if instruccion == 'crear_producto':
                         nombre, cantidad, precio_unitario, ubicacion = detalles.split(';')
-                        nuevo_producto = {
-                            "nombre": nombre,
-                            "cantidad": cantidad,
-                            "precio_unitario": precio_unitario,
-                            "ubicacion": ubicacion
-                        }
+                        nuevo_producto = Producto(nombre, cantidad, precio_unitario, ubicacion)
                         self.inventario.append(nuevo_producto)
                         texto = 'Inventario actualizado exitosamente.'
-                    else:
-                        texto = 'El archivo no ha sido escrito correctamente.'
             
             return texto
-            
         except FileNotFoundError:
             print('El archivo .inv no se encontró. Verifica la ubicación del archivo.')    
     
@@ -36,10 +33,10 @@ class Producto_DAO():
         for producto in self.inventario:
             i += 1
             print('---------------- ', i, ' ----------------')
-            print('Nombre: ', producto['nombre'])
-            print('Cantidad: ', producto['cantidad'])
-            print('Precio Unitario: ', producto['precio_unitario'])
-            print('Ubicacion: ', producto['ubicacion'])
+            print('Nombre: ', producto.nombre)
+            print('Cantidad: ', producto.cantidad)
+            print('Precio Unitario: ', producto.precio_unitario)
+            print('Ubicacion: ', producto.ubicacion)
             print()
             
     
